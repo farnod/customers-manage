c6f2cff24dd5e590bf4e504b1b3d5dbbb89e2ca2
farnoud <farnoodcc@gmail.com>
2023-04-21 19:01:22 +0430

    post api set

--- pages/api/customer/index.js
+++ pages/api/customer/index.js
@@ -1,25 +1,59 @@
-// Import the connectDB function from the utils folder
+import Customer from "@/models/customer";
 import connectDB from "@/utils/connectDB";
 
-// Define an async function called 'handler' with two parameters, a req (request) and res (response)
 export default async function handler(req, res) {
-  // Check if the HTTP method is a POST request
-  if (req.method === "POST") {
-	// Get the body of the request and assign it to the reqObject variable
-	const reqObject = await req.body;
+	try {
+		await connectDB();
+	} catch (err) {
+		console.log(err);
+		return res
+			.status(500)
+			.json({ status: "failed", message: "error in connecting DB" });
+	}
+	if (req.method === "POST") {
+		const data = req.body.data;
 
-	// Call the connectDB function to establish a connection to the database
-	await connectDB();
+		if (!data.name || !data.lastName || !data.email)
+			return res.status(400).json({
+				status: "failed",
+				message: "Invalid data",
+			});
 
-	// Send a JSON response with a 201 status code indicating that the data was successfully created
-	res.status(201).json({
-	  status: "success",
-	  message: "data created",
-	  container: reqObject,
-	});
-
-	// Log the reqObject to the console for debugging purposes
-	console.log(reqObject);
-  }
+		try {
+			const customer = await Customer.create(data);
+			res
+				.status(201)
+				.json({ status: "success", message: "data created", data: customer  se});
+		} catch (err) {
+			console.log(err);
+			return res
+				.status(500)
+				.json({ status: "failed", message: "Error in storing in DB" });
+		}
+	}
 }
 
+// // Import the connectDB function from the utils folder
+// import connectDB from "@/utils/connectDB";
+
+// // Define an async function called 'handler' with two parameters, a req (request) and res (response)
+// export default async function handler(req, res) {
+//   // Check if the HTTP method is a POST request
+//   if (req.method === "POST") {
+// 	// Get the body of the request and assign it to the reqObject variable
+// 	const reqObject = await req.body;
+
+// 	// Call the connectDB function to establish a connection to the database
+// 	await connectDB();
+
+// 	// Send a JSON response with a 201 status code indicating that the data was successfully created
+// 	res.status(201).json({
+// 	  status: "success",
+// 	  message: "data created",
+// 	  container: reqObject,
+// 	});
+
+// 	// Log the reqObject to the console for debugging purposes
+// 	console.log(reqObject);
+//   }
+// }
--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,9 +1,9 @@
-import mongoose, { mongo  } from "mongoose";
+import mongoose from "mongoose";
 
 async function connectDB() {
 	if (mongoose.connections[0].readyState) return;
+	mongoose.set("strictQuery", false);
 	await mongoose.connect(process.env.MONGO_URI);
 	console.log("connectDB");
 }
 export default connectDB;
-
